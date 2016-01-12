2f0b5b36cf09a863377b73b0e08a6005f8f66be2
dp.wang <dp.wang@dianping.com>
2016-01-12 14:34:50 +0800

    delete

--- squirrel-web/src/main/java/com/dianping/cache/controller/IndexController.java
+++ squirrel-web/src/main/java/com/dianping/cache/controller/IndexController.java
@@ -1,37 +1,18 @@
 package com.dianping.cache.controller;
 
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.dianping.cache.monitor.NotifyManager;
-
 
 @Controller
 public class IndexController extends AbstractMenuController{
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(IndexController.class);
-	
 	@RequestMapping(value = "/")
-	public ModelAndView allApps(HttpServletRequest request,
-			HttpServletResponse response) { 
+	public ModelAndView allApps() {
 		return new ModelAndView("cache/config",createViewMap());
 	}
 	
-	@RequestMapping(value = "/send")
-	public void sendsms(HttpServletRequest request,
-			HttpServletResponse response) { 
-		NotifyManager.getInstance().notifySms2("Justtest");
-	}
 
 	@Override
 	protected String getMenu() {
