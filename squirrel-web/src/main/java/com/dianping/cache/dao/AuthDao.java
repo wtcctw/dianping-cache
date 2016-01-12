db9663ef445db5f9aa7dc35aa604e6948b15e6c9
Enlight <fv3386@gmail.com>
2016-01-12 15:30:05 +0800

    refactor task

--- squirrel-web/src/main/java/com/dianping/cache/dao/AuthDao.java
+++ squirrel-web/src/main/java/com/dianping/cache/dao/AuthDao.java
@@ -6,7 +6,11 @@ import com.dianping.cache.entity.Auth;
  * Created by dp on 16/1/9.
  */
 public interface AuthDao {
+    
     void insert(Auth auth);
+
     void update(Auth auth);
+
     void delete(Auth auth);
+    
 }
