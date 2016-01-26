400dc015de2221c5ce63d2bce3618c95c355bb18
z3635363 <245767493@qq.com>
2016-01-26 16:26:41 +0800

    delete category

--- squirrel-web/src/main/java/com/dianping/squirrel/task/ClearCategoryTask.java
+++ squirrel-web/src/main/java/com/dianping/squirrel/task/ClearCategoryTask.java
@@ -9,7 +9,11 @@ import com.dianping.squirrel.client.impl.redis.RedisStoreClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import redis.clients.jedis.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by thunder on 16/1/6.
@@ -23,7 +27,7 @@ public class ClearCategoryTask extends AbstractTask {
     @Autowired
     private RdbService rdbService;
 
-    public ClearCategoryTask(){
+    public ClearCategoryTask() {
     }
 
     public ClearCategoryTask(String category) {
@@ -33,7 +37,7 @@ public class ClearCategoryTask extends AbstractTask {
 
     @Override
     String getTaskType() {
-        return TASKTYPE;
+        return TASKTYPE + " : " + this.category;
     }
 
     @Override
@@ -43,64 +47,92 @@ public class ClearCategoryTask extends AbstractTask {
 
     @Override
     long getTaskMaxStat() {
-      try {
-        RdbService.TotalStat totalStat = rdbService.getCategoryMergeStat(category);
-        return totalStat.count;
-      } catch (Exception e) {
-        return 2000;
-      }
+        try {
+            RdbService.TotalStat totalStat = rdbService.getCategoryMergeStat(category);
+            return totalStat.count;
+        } catch (Exception e) {
+            return 2000;
+        }
     }
 
     @Override
     public void taskRun() {
+
         if (storeClient == null || !(storeClient instanceof RedisStoreClient)) {
             return;
         }
+        ExecutorService executorService = Executors.newCachedThreadPool();
         JedisCluster jedisCluster = ((RedisStoreClient) storeClient).getJedisClient();
         Set<HostAndPort> nodes = jedisCluster.getCluserNodesHostAndPort();
-        long stat = 0;
-        int step = 2000;
+        final AtomicLong stat = new AtomicLong(0);
+        final int step = 2000;
+        Set<HostAndPort> masters = new HashSet<HostAndPort>();
+
         for (HostAndPort node : nodes) {
-            System.out.println(node.getHost() + " " + node.getPort());
             Jedis jedis = new Jedis(node.getHost(), node.getPort());
             try {
                 String info = jedis.info("Replication");
-                if(info.indexOf("role:master") < 0)
+                if (info.indexOf("role:master") < 0)
                     continue;
+                masters.add(node);
             } catch (Throwable t) {
                 continue;
             }
-            ScanParams scanParams = new ScanParams();
-            scanParams.match(category + "*");
-            scanParams.count(step);
-            ScanResult<String> result;
-            try {
-                result = jedis.scan("0", scanParams);
-            } catch (Throwable e) {
-                continue;
-            }
-            boolean a = Thread.currentThread().isInterrupted();
-            boolean b = result.getStringCursor().equals("0");
-            while (!Thread.currentThread().isInterrupted() && !b) {
-                try {
-                    for (String key : result.getResult()) {
-                        jedis.del(key);
-                        stat += 1;
-                    }
-                    if(stat % 1000 == 0)
-                        this.updateStat((int)stat);
-                    result = jedis.scan(result.getStringCursor(), scanParams);
-                } catch (Throwable t) {
-                    continue;
-                }
-            }
-            this.updateStat((int)stat);
         }
+
+        final CountDownLatch latch = new CountDownLatch(masters.size());
+        ArrayList<Future> masterTask = new ArrayList<Future>();
+
+        for (final HostAndPort node : masters) {
+            Future f = executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    Jedis jedis = new Jedis(node.getHost(), node.getPort());
+                    ScanParams scanParams = new ScanParams();
+                    scanParams.match(category + "*");
+                    scanParams.count(step);
+                    ScanResult<String> result;
+                    result = jedis.scan("0", scanParams);
+                    boolean isInterrupted = Thread.currentThread().isInterrupted();
+                    boolean isEnd = result.getStringCursor().equals("0");
+                    while (!isInterrupted && !isEnd) {
+                        try {
+                            for (String key : result.getResult()) {
+                                jedis.del(key);
+                                stat.addAndGet(1);
+                                if (stat.get() % 10000 == 0) {
+                                    synchronized (stat) {
+                                        if(stat.get() % 10000 == 0)
+                                            ClearCategoryTask.this.updateStat((int) stat.get());
+                                    }
+                                }
+                            }
+                            result = jedis.scan(result.getStringCursor(), scanParams);
+                        } catch (Throwable t) {
+                            continue;
+                        }
+                    }
+                    latch.countDown();
+                }
+            });
+            masterTask.add(f);
+        }
+        while(latch.getCount() != 0) {
+            try {
+                latch.await(15, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                for(Future f : masterTask) {
+                    f.cancel(true);
+                }
+                executorService.shutdownNow();
+            }
+        }
+        executorService.shutdownNow();
     }
 
     public static void main(String[] args) {
         StoreClient storeClient = StoreClientFactory.getStoreClient();
-        for(int i = 0; i < 2000; i++) {
+        for (int i = 0; i < 2000; i++) {
             storeClient.set(new StoreKey("redis-del", i), i);
         }
         // 192.168.217.36 7004
