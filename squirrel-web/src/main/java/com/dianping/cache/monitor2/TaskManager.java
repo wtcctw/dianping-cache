3d1300a86bd60cb5a2760fa9d013e08a68e535d0
z3635363 <245767493@qq.com>
2016-02-03 16:08:12 +0800

    0.1

--- squirrel-web/src/main/java/com/dianping/cache/monitor2/TaskManager.java
+++ squirrel-web/src/main/java/com/dianping/cache/monitor2/TaskManager.java
@@ -6,23 +6,20 @@ import com.dianping.cache.monitor.CuratorManager;
 import com.dianping.cache.monitor.MemberMonitor;
 import com.dianping.cache.monitor2.ServerState;
 import com.dianping.squirrel.common.util.JsonUtils;
+import net.sf.ehcache.exceptionhandler.CacheExceptionHandler;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.RetryNTimes;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import sun.misc.Cache;
 
 import java.io.UnsupportedEncodingException;
 import java.lang.management.BufferPoolMXBean;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
+import java.util.concurrent.*;
 
 /**
  * Created by thunder on 16/1/29.
@@ -41,49 +38,37 @@ public class TaskManager {
 
     private ExecutorService taskRunnerThreadPool;
 
+    private ExecutorService judgeRunnerThreadPool;
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskRunner.class);
+
     private double percent = 0.5;
 
     public TaskManager() {
         this.memberMonitor = new MemberMonitor();
         monitorThreadPool = new ScheduledThreadPoolExecutor(1);
         taskRunnerThreadPool = Executors.newCachedThreadPool();
+        judgeRunnerThreadPool = Executors.newCachedThreadPool();
         CuratorManager.getInstance().ensurePath(Constants.MANAGER_PATH);
         curatorClient = CuratorManager.getInstance().getCuratorClient();
-    }
-
-    public void doCheck() {
-
     }
 
     String concatZkPath(String s1, String s2) {
         return s1 + "/" + s2;
     }
 
-    private void zkDelete(String path) {
-        try {
-            List<String> children = curatorClient.getChildren().forPath(path);
-            if (children == null || children.size() == 0) {
-                curatorClient.delete().forPath(path);
-                return ;
-            }
-            for (String child : children) {
-                zkDelete(concatZkPath(path, child));
-            }
+    private void zkDelete(String path) throws Exception {
+        List<String> children = curatorClient.getChildren().forPath(path);
+        if (children == null || children.size() == 0) {
             curatorClient.delete().forPath(path);
-        } catch (Exception e) {
-            e.printStackTrace();
+            return ;
         }
+        for (String child : children) {
+            zkDelete(concatZkPath(path, child));
+        }
+        curatorClient.delete().forPath(path);
     }
 
-    private void filterServers() throws Exception {
-        List<String> clusters = curatorClient.getChildren().forPath(Constants.MANAGER_PATH);
-        List<String> clusterInUse = curatorClient.getChildren().forPath(Constants.SERVICE_PATH);
-        for(String inUse : clusterInUse) {
-            if(!clusters.contains(inUse)) {
-                zkDelete(concatZkPath(Constants.SERVICE_PATH, inUse));
-            }
-        }
-    }
     private CacheConfiguration getServersFromZkData(String path) throws Exception {
         byte[] data = curatorClient.getData().watched().forPath(path);
         if(data == null || data.length==0) {
@@ -107,7 +92,11 @@ public class TaskManager {
         List<String> clusters = getManagerClusters();
         Map<String, CacheConfiguration> map = new HashMap<String, CacheConfiguration>();
         for(String cluster : clusters) {
+            if(!cluster.startsWith("memcached"))
+                continue;
             CacheConfiguration config = getManagerClusterCinfiguration(cluster);
+            if(config == null)
+                continue;
             map.put(cluster, config);
         }
         return map;
@@ -126,7 +115,13 @@ public class TaskManager {
     private void setServiceConfiguration(String cluster, CacheConfiguration config) throws Exception {
         // : 设置线上的机器集群状态
         String result = JsonUtils.toStr(config);
-        curatorClient.setData().forPath(concatZkPath(Constants.SERVICE_PATH, cluster), result.getBytes("UTF-8"));
+
+        String path = concatZkPath(Constants.SERVICE_PATH, cluster);
+        if(curatorClient.checkExists().forPath(concatZkPath(Constants.SERVICE_PATH, cluster)) == null) {
+            curatorClient.create().creatingParentsIfNeeded().forPath(path, result.getBytes("UTF-8"));
+        } else {
+            curatorClient.setData().forPath(concatZkPath(Constants.SERVICE_PATH, cluster), result.getBytes("UTF-8"));
+        }
     }
 
     private void setManagerConfiguration(String cluster, CacheConfiguration config) throws Exception {
@@ -138,16 +133,33 @@ public class TaskManager {
 
     private List<String> getManagerClusters() throws Exception {
         // : 删掉 map 里面的多余项
-        return curatorClient.getChildren().forPath(Constants.MANAGER_PATH);
+        List<String> result = new ArrayList<String>();
+        List<String> clusters = curatorClient.getChildren().forPath(Constants.MANAGER_PATH);
+        for(String cluster : clusters) {
+            if(!cluster.startsWith("memcached"))
+                continue;
+            CacheConfiguration config = getManagerClusterCinfiguration(cluster);
+            if(config == null || config.getServerList() == null)
+                continue;
+            result.add(cluster);
+        }
+        return result;
     }
+
     private List<String> getServiceClusters() throws Exception {
         if(curatorClient.checkExists().forPath(Constants.SERVICE_PATH) == null)
             curatorClient.create().forPath(Constants.SERVICE_PATH);
         return curatorClient.getChildren().forPath(Constants.SERVICE_PATH);
     }
+
     private void addServiceCluster(CacheConfiguration cacheConfiguration) throws Exception {
         String path = concatZkPath(Constants.SERVICE_PATH, cacheConfiguration.getCacheKey());
         curatorClient.create().creatingParentsIfNeeded().forPath(path, JsonUtils.toStr(cacheConfiguration).getBytes("UTF-8"));
+    }
+
+    private void removeServiceCluster(String cluster) throws Exception {
+        String path = concatZkPath(Constants.SERVICE_PATH, cluster);
+        zkDelete(path);
     }
 
     void filterLiveNodes() throws Exception {
@@ -155,64 +167,77 @@ public class TaskManager {
         Map<String, CacheConfiguration> managerConfig = getManagerClusterConfigurationMap();
         Map<String, CacheConfiguration> serviceConfig = getServiceClusterConfigurationMap();
 
+        // 管理员删除了 cluster
+        for(Map.Entry<String, CacheConfiguration> entry : serviceConfig.entrySet()) {
+            if(!managerConfig.keySet().contains(entry.getKey())) {
+                removeServiceCluster(entry.getKey());
+            }
+        }
+
+        // 管理员添加了 cluster 默认是上线的
+        for(Map.Entry<String, CacheConfiguration> entry : managerConfig.entrySet()) {
+            if(!serviceConfig.keySet().contains(entry.getKey())) {
+                setServiceConfiguration(entry.getKey(), entry.getValue());
+            }
+        }
+        // 重新同步一下数据
+        serviceConfig = getServiceClusterConfigurationMap();
+
         // 根据 manager 中的数据同步线上的数据
-        for (Map.Entry<String, CacheConfiguration> entry : managerConfig.entrySet()) {
+        for(Map.Entry<String, CacheConfiguration> entry : managerConfig.entrySet()) {
+
             String cluster = entry.getKey();
 
             CacheConfiguration managerClusterConfig = entry.getValue();
             CacheConfiguration serviceClusterConfig = serviceConfig.get(cluster);
 
             List<String> managerServers = managerClusterConfig.getServerList();
+            boolean change = false;
 
-            // 管理员添加了 cluster
-            // TODO: 管理员删除的 cluster
-            if (serviceClusterConfig == null) {
-                CacheConfiguration newCluster = managerClusterConfig;
-                addServiceCluster(newCluster);
-            } else {
+            // 去掉 service 中管理员去掉了的 cluster 中的某些机器
+            List<String> serviceServers = serviceClusterConfig.getServerList();
+            List<String> forRemoveFromServiceServer = new ArrayList<String>();
 
-                boolean change = false;
-                // 管理员去掉了 cluster 中的某些机器
-                // TODO : 边遍历边删除 去掉
-                List<String> serviceServers = serviceClusterConfig.getServerList();
-                List<String> forRemoveFromServiceServer = new ArrayList<String>();
+            if(serviceServers == null)
+                continue;
 
-                for (String server : serviceServers) {
-                    if (!managerServers.contains(server)) {
-                        forRemoveFromServiceServer.add(server);
-                        change = true;
-                    }
+            for (String server : serviceServers) {
+                if (!managerServers.contains(server)) {
+                    forRemoveFromServiceServer.add(server);
+                    change = true;
                 }
+            }
 
-                for(String s : forRemoveFromServiceServer) {
-                    serviceServers.remove(s);
-                }
+            for(String s : forRemoveFromServiceServer) {
+                serviceServers.remove(s);
+            }
 
-                List<String> froAddFromServiceServer = new ArrayList<String>();
-                // 管理员添加了 cluster 中的某些机器
-                for (String server : managerServers) {
-                    if (!serviceServers.contains(server)) {
-                        froAddFromServiceServer.add(server);
-                        change = true;
-                    }
+            List<String> forAddFromServiceServer = new ArrayList<String>();
+            // 管理员添加了 cluster 中的某些机器
+            for (String server : managerServers) {
+                if (!serviceServers.contains(server)) {
+                    forAddFromServiceServer.add(server);
+                    change = true;
                 }
-                serviceServers.addAll(froAddFromServiceServer);
-                // 使得修改生效
-                if(change) {
-                    serviceClusterConfig.setServerList(serviceServers);
-                    setServiceConfiguration(cluster, serviceClusterConfig);
-                }
+            }
+            serviceServers.addAll(forAddFromServiceServer);
+            // 使得修改生效
+            if(change) {
+                serviceClusterConfig.setServerList(serviceServers);
+                setServiceConfiguration(cluster, serviceClusterConfig);
             }
         }
 
         //清理 MARK DOWN 和 MARK UP 只需要将多余的节点( manager 没有配置过的 )删除
-        List<String> allManagerServers = new ArrayList<String>();
+        Set<String> allManagerServers = new HashSet<String>();// 唯一的 key
         for (CacheConfiguration cacheConfiguration : managerConfig.values()) {
+            if(cacheConfiguration.getServerList() == null)
+                continue;
             allManagerServers.addAll(cacheConfiguration.getServerList());
         }
 
-        List<String> markDownChildren = curatorClient.getChildren().forPath(Constants.MONITOR_MARKDOWN_PATH);
-        List<String> markUpChildren = curatorClient.getChildren().forPath(Constants.MONITOR_MARKUP_PATH);
+        List<String> markDownChildren = getMarkDownChildren();
+        List<String> markUpChildren = getMarkUpChildren();
 
         for (String hostPort : markDownChildren) {
             if (!allManagerServers.contains(hostPort))
@@ -226,6 +251,17 @@ public class TaskManager {
 
     }
 
+
+    private List<String> getMarkDownChildren() throws Exception {
+        return curatorClient.getChildren().forPath(Constants.MONITOR_MARKDOWN_PATH);
+    }
+
+
+    private List<String> getMarkUpChildren() throws Exception {
+        return curatorClient.getChildren().forPath(Constants.MONITOR_MARKUP_PATH);
+    }
+
+
     private void changeNodeMode(boolean add, String cluster) throws Exception {
         CacheConfiguration managerConfig = getManagerClusterCinfiguration(cluster);
         CacheConfiguration serviceConfig = getServiceClusterCinfiguration(cluster);
@@ -233,26 +269,28 @@ public class TaskManager {
         List<String> managerServers = managerConfig.getServerList();
         boolean change = false;
         for(String server : managerServers) {
-            String path;
+
+            List<String> internalChildren;
+
             if(add) {
-                path = concatZkPath(Constants.MONITOR_MARKUP_PATH, server);
+                internalChildren = getMarkUpChildren();
             } else {
-                path = concatZkPath(Constants.MONITOR_MARKDOWN_PATH, server);
+                internalChildren = getMarkDownChildren();
             }
 
-            if(curatorClient.checkExists().forPath(path) != null) {
-                List<String> children = curatorClient.getChildren().forPath(path);
-                // TODO : 这个地方是测试 所以直接打开
-                //if(children.size() > memberMonitor.getMemberCount() * percent) {
-                if (add && !serviceServers.contains(server)) {
+            List<String> children = internalChildren;
+            if(children.size() > memberMonitor.getMemberCount() * percent) {
+                if (add && serviceServers != null && !serviceServers.contains(server)) {
+                    if(serviceServers == null) {
+                        serviceServers = new ArrayList<String>();
+                    }
                     serviceServers.add(server);
                     change = true;
                 }
-                if (!add && serviceServers.contains(server)) {
+                if (!add && serviceServers != null && serviceServers.contains(server)) {
                     serviceServers.remove(server);
                     change = true;
                 }
-                //}
             }
         }
         if(change) {
@@ -271,21 +309,43 @@ public class TaskManager {
     }
 
     private void judgeMachineState() throws Exception {
+        curatorClient.create().creatingParentsIfNeeded().forPath(Constants.MONITOR_JUDGE_LOCK);
         try {
-            curatorClient.create().creatingParentsIfNeeded().forPath(Constants.MONITOR_JUDGE_LOCK);
-            try {
-                filterLiveNodes(); // 每个周期进行一次就可以了
-                // TODO: 这个地方对不对？要先找到 manager 配置的 cluster 然后根据 cluster 找到每个 cluster 的机器
-                List<String> managerClusters = getManagerClusters();
-                for (String cluster : managerClusters) {
-                    addLiveNode(cluster);
-                    removeDeadNode(cluster);
-                }
-            } finally {
-                curatorClient.delete().forPath(Constants.MONITOR_JUDGE_LOCK);
+            filterLiveNodes(); // 把管理员的更改同步到内部状态中
+            List<String> managerClusters = getManagerClusters();
+
+            // 把监测结果同步到 service path 中
+            final CountDownLatch latch = new CountDownLatch(managerClusters.size() * 2);
+            for (final String cluster : managerClusters) {
+                // 把内部监测信息反馈到状态中
+                judgeRunnerThreadPool.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            addLiveNode(cluster);
+                        } catch (Exception e) {
+                            logger.error("addLiveNode failed  cluster : " + cluster + " " + e.getMessage());
+                        } finally {
+                            latch.countDown();
+                        }
+                    }
+                });
+                judgeRunnerThreadPool.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            removeDeadNode(cluster);
+                        } catch (Exception e) {
+                            logger.error("removeDeadNode failed  cluster : " + cluster + " " + e.getMessage());
+                        } finally {
+                            latch.countDown();
+                        }
+                    }
+                });
             }
-        } catch (Exception e) {
-            System.out.println(e.toString() + e.getLocalizedMessage());
+            latch.await();
+        } finally {
+            curatorClient.delete().forPath(Constants.MONITOR_JUDGE_LOCK);
         }
     }
 
@@ -324,19 +384,15 @@ public class TaskManager {
             public void run() {
                 try {
                     List<String> clusters = getManagerClusters();
+                    Map<String, Boolean> mp = new HashMap<String, Boolean>(); // 一个 server 可能被配置到多个 cate
                     for(String cluster : clusters) {
-                        if(!cluster.startsWith("memcached"))
-                            continue;
                         CacheConfiguration configuration = getManagerClusterCinfiguration(cluster);
                         List<String> servers = configuration.getServerList();
-                        Map<String, Boolean> mp = new HashMap<String, Boolean>();
                         for(String server : servers) {
                             if(mp.get(server) != null) {
                                 continue;
-                            } else {
-                                Boolean b = true;
-                                mp.put(server, b);
                             }
+                            mp.put(server, new Boolean(true));
                             if(serverStatMap.get(server) == null)
                                 serverStatMap.put(server, new ServerState(server));
                             TaskRunner taskRunner = new TaskRunner(serverStatMap.get(server));
@@ -345,22 +401,13 @@ public class TaskManager {
                         judgeMachineState();
                     }
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    logger.error("task running error " + e.getMessage());
                 }
             }
         };
         // TODO: 这个地方是间隔
-        int interval = 1000;
+        int interval = 5000;
         monitorThreadPool.scheduleWithFixedDelay(task, interval, interval, TimeUnit.MILLISECONDS);
-    }
-
-    public void deleteLocks() {
-        try {
-            curatorClient.delete().forPath(Constants.INIT_STAT_LOCK);
-            curatorClient.delete().forPath(Constants.MONITOR_JUDGE_LOCK);
-        } catch (Exception e) {
-
-        }
     }
 
     private void copyToAlpha(String betaPath, String alphaPath, CuratorFramework betaClient, CuratorFramework alphaClient) throws Exception {
@@ -444,9 +491,7 @@ public class TaskManager {
     }
 
     public static void main(String[] args) {
-
         try {
-
             TaskManager taskManager = new TaskManager();
             taskManager.removeAllData();
             taskManager.prepareBetaData();
